8198c7649c7bd017c0067c058260bfc6597f3458
hmiyachi <hmiyachi@gijuhrdsad.local>
2020-08-29 17:44:29 +0900

    Modified Personal

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>SESエンジニア、上善如水</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
